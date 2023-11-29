6805fc99eadf686aa554a0b86dd2521cb5bcc589
deeme <github.io@gmail.com>
2023-11-29 11:44:29 +0800

    更改提示

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import { DisplayImages } from "./Images";
 import ImageDownloader from "./ImagesDownload";
@@ -10,19 +10,35 @@ function App() {
   const [loading, setLoading] = useState(false);
   const [placeholder, setPlaceholder] = useState("Search Bears with Paint Brushes the Starry Night, painted by Vincent Van Gogh...");
   const [quantity, setQuantity] = useState(5);
-  const [imageSize, setImageSize] = useState("1024x1024");
   const [model, setModel] = useState("sdxl");
   const [maxQuantity, setMaxQuantity] = useState(5);
 
+  const [imageModels, setImageModels] = useState([]);
+
+  useEffect(() => {
+    const fetchImageModels = async () => {
+      const apiUrl = `${import.meta.env.VITE_OPEN_AI_BASE}/v1/models`;
+      try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        const imageModels = data.data.filter((model) => model.max_images);
+        setImageModels(imageModels);
+      } catch (error) {
+        console.error("Error fetching image models:", error);
+      }
+    };
+
+    fetchImageModels();
+  }, []);
+
   const generateImage = async () => {
     setRequestError(false);
-    setImageSize(imageSize);
     setPlaceholder(`Search ${prompt}...`);
     setPrompt(prompt);
     setLoading(true);
 
-    const apiUrl = import.meta.env.VITE_Open_AI_Url;
-    const openaiApiKey = import.meta.env.VITE_Open_AI_Key;
+    const apiUrl = `${import.meta.env.VITE_OPEN_AI_BASE}/v1/images/generations`;
+    const openaiApiKey = import.meta.env.VITE_OPEN_AI_KEY;
     try {
       const response = await fetch(apiUrl, {
         method: "POST",
@@ -34,17 +50,13 @@ function App() {
           model: model,
           prompt: prompt,
           n: quantity,
-          size: imageSize,
         }),
       });
 
       if (!response.ok) {
         setRequestError(true);
-        try {
-          setRequestErrorMessage(await JSON.parse(response.text()).error);
-        } catch (e) {
-          setRequestErrorMessage(await response.text());
-        }
+        const errorMessage = await response.json();
+        setRequestErrorMessage(await errorMessage.error.message);
       }
 
       const data = await response.json();
@@ -64,36 +76,21 @@ function App() {
   };
 
   const handleModelSelect = (e) => {
-    setModel(e.target.value);
-    const modelMaxImages = {
-      "kandinsky-v2.2": 10,
-      "kandinsky-v2": 10,
-      sdxl: 5,
-      "stable-diffusion-v2.1": 10,
-      "stable-diffusion-v1.5": 10,
-      "deepfloyd-if": 4,
-      "dalle-3": 10,
-			"openjourney-xl": 4,
-			"openjourney-v4": 4,
-			"dreamshaper": 4,
-			"majicmixsombre": 4,
-			"pastelMixAnime": 4,
-			"absoluteReality": 4,
-			"anything": 4,
-			"meinamix": 4,
-			"deliberate": 4,
-			"revAnimated": 4,
-			"realisticVision": 4,
-    };
-    setQuantity(Math.min(quantity, modelMaxImages[e.target.value]));
-    setMaxQuantity(modelMaxImages[e.target.value]);
+    const selectedModelId = e.target.value;
+    const selectedModel = imageModels.find((model) => model.id === selectedModelId);
+    setModel(selectedModelId);
+
+    if (selectedModel) {
+      setMaxQuantity(selectedModel.max_images);
+    }
+    setQuantity(Math.min(quantity, maxQuantity));
   };
 
   return (
     <div className="app-main">
       {loading ? (
         <>
-          <h2>æ­£åœ¨ç”Ÿæˆæ‚¨ç‹¬ç‰¹çš„å›¾ç‰‡...è¯·è€å¿ƒç­‰å¾…ï¼</h2>
+          <h2>ÕıÔÚÎªÄú´òÔì¸öĞÔ»¯Í¼Æ¬... ÇëÄÍĞÄµÈ´ı!</h2>
           <div className="lds-ripple">
             <div></div>
             <div></div>
@@ -105,38 +102,25 @@ function App() {
         <>
           {requestError ? <div className="alert">{requestErrorMessage}</div> : null}
 
-          <h2>åªæœ‰DALLEæ”¯æŒä¸­æ–‡æè¿°</h2>
+          <h2>ÀûÓÃ¶àÖÖÈË¹¤ÖÇÄÜÄ£ĞÍ´´½¨Í¼Æ¬</h2>
           <div className="select-container">
             <select value={model} onChange={handleModelSelect}>
-              <option value="kandinsky-v2.2">Kandinsky 2.2</option>
-              <option value="kandinsky-v2">Kandinsky 2</option>
-              <option value="sdxl">SDXL</option>
-              <option value="stable-diffusion-v2.1">Stable Diffusion 2.1</option>
-              <option value="stable-diffusion-v1.5">Stable Diffusion 1.5</option>
-              <option value="deepfloyd-if">Deepfloyd IF</option>
-              <option value="dalle-3">DALL-E</option>
-              <option value="openjourney-xl">openjourney-xl</option>
-              <option value="openjourney-v4">openjourney-v4</option>
-              <option value="dreamshaper">dreamshaper</option>
-              <option value="majicmixsombre">majicmixsombre</option>
-              <option value="pastelMixAnime">pastelMixAnime</option>
-              <option value="absoluteReality">absoluteReality</option>
-              <option value="anything">anything</option>
-              <option value="meinamix">meinamix</option>
-              <option value="deliberate">deliberate</option>
-              <option value="revAnimated">revAnimated</option>
-              <option value="realisticVision">realisticVision</option>
+              {imageModels.map((imageModel) => (
+                <option key={imageModel.id} value={imageModel.id}>
+                  {imageModel.id}
+                </option>
+              ))}
             </select>
 
             <ImageDownloader />
           </div>
-
 
           <textarea
             className="app-input"
             placeholder={placeholder}
             onChange={(e) => setPrompt(e.target.value)}
             rows="10"
+            cols="40"
             autoFocus
             onKeyDown={(e) => {
               if (e.key === "Enter") {
@@ -145,15 +129,13 @@ function App() {
               }
             }}
           />
-
-
-          <label htmlFor="quantity">å›¾ç‰‡æ•°é‡:</label>
+          <label htmlFor="quantity">Í¼Æ¬ÊıÁ¿:</label>
           <input id="quantity" type="range" min="1" max={maxQuantity} value={quantity} onChange={(e) => setQuantity(parseInt(e.target.value))} />
           <span>{quantity}</span>
 
           <br />
-          <button onClick={generateImage} id="generate" className="text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center">
-            ç‚¹å‡»ç”Ÿæˆ
+          <button onClick={generateImage} id="generate">
+            Éú³ÉÍ¼Ïñ
           </button>
           <DisplayImages />
         </>
